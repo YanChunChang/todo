faa37213331425d1c7a75b2893d4ac6cc93727db
chun <yanchun.chang5@gmail.com>
2026-02-11 17:43:20 +0100

    feat: add a root URL path that returns a simple backend status message

--- todo-backend/backend/backend/urls.py
+++ todo-backend/backend/backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Backend läuft")),
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
 ]
